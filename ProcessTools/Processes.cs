42aa6291adbe1d3de1bc006f6d6375b0595b06e9
Brent Scriver <bscriver@oneoddsock.com>
2016-05-11 23:40:43 -0400

    First pass for necessary p/invoke calls for querying the process list as I do in PSView2.

---
+++ ProcessTools/Processes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTools
+{
+    /// <summary>
+    /// Provides the methods to query the processes on the system.
+    /// </summary>
+    public class Processes
+    {
+        /// <summary>
+        /// Returns the current process list.
+        /// </summary>
+        public static SortedList<uint, ProcessInformation> GetProcessList(string idleProcessName, string systemName)
+        {
+            return null;
+        }
+
+
+        /// <summary>
+        /// Acquires the full path to the process
+        /// </summary>
+        private void GetProcessFullPath(ProcessInformation processInfo)
+        {
+        }
+
+        /// <summary>
+        /// Acquires the name, path, and other details for the process
+        /// </summary>
+        private ProcessInformation GetProcessInfo(uint processID, string idleProcessName, string systemName)
+        {
+            return null;
+        }
+    }
+}
---
+++ ProcessTools/Windows/Interop.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ProcessTools.Windows
+{
+    [Flags]
+    internal enum ProcessAccessFlags : uint
+    {
+        All = 0x001F0FFF,
+        Terminate = 0x00000001,
+        CreateThread = 0x00000002,
+        VirtualMemoryOperation = 0x00000008,
+        VirtualMemoryRead = 0x00000010,
+        VirtualMemoryWrite = 0x00000020,
+        DuplicateHandle = 0x00000040,
+        CreateProcess = 0x000000080,
+        SetQuota = 0x00000100,
+        SetInformation = 0x00000200,
+        QueryInformation = 0x00000400,
+        QueryLimitedInformation = 0x00001000,
+        Synchronize = 0x00100000
+    }
+
+    [Flags]
+    internal enum SnapshotFlags : uint
+    {
+        HeapList = 0x00000001,
+        Process = 0x00000002,
+        Thread = 0x00000004,
+        Module = 0x00000008,
+        Module32 = 0x00000010,
+        All = (HeapList | Process | Thread | Module),
+        Inherit = 0x80000000,
+        NoHeaps = 0x40000000
+
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PROCESSENTRY32
+    {
+        public uint dwSize;
+        public uint cntUsage;
+        public uint th32ProcessID;
+        public IntPtr th32DefaultHeapID;
+        public uint th32ModuleID;
+        public uint cntThreads;
+        public uint th32ParentProcessID;
+        public int pcPriClassBase;
+        public uint dwFlags;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string szExeFile;
+    };
+
+    internal delegate bool EnumDesktopWindowsDelegate(IntPtr hWnd, int lParam);
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    internal struct ENUM_SERVICE_STATUS_PROCESS
+    {
+        internal static readonly int SizePack4 = Marshal.SizeOf(typeof(ENUM_SERVICE_STATUS_PROCESS));
+
+        /// <summary>
+        /// sizeof(ENUM_SERVICE_STATUS_PROCESS) allow Packing of 8 on 64 bit machines
+        /// </summary>
+        internal static readonly int SizePack8 = Marshal.SizeOf(typeof(ENUM_SERVICE_STATUS_PROCESS)) + 4;
+
+        [MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+        internal string pServiceName;
+
+        [MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+        internal string pDisplayName;
+
+        internal SERVICE_STATUS_PROCESS ServiceStatus;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SERVICE_STATUS_PROCESS
+    {
+        public int serviceType;
+        public int currentState;
+        public int controlsAccepted;
+        public int win32ExitCode;
+        public int serviceSpecificExitCode;
+        public int checkPoint;
+        public int waitHint;
+        public int processId;
+        public int serviceFlags;
+    }
+    internal class Interop
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, uint th32ProcessID);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDesktopWindowsDelegate lpfn, IntPtr lParam);
+
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool EnumServicesStatusEx(IntPtr hSCManager,
+            int infoLevel, int dwServiceType,
+            int dwServiceState, IntPtr lpServices, UInt32 cbBufSize,
+            out uint pcbBytesNeeded, out uint lpServicesReturned,
+            ref uint lpResumeHandle, string pszGroupName);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseServiceHandle(IntPtr hSCObject);
+
+        [DllImport("Psapi.dll", SetLastError = true)]
+        public static extern bool EnumProcesses([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U4)] [In][Out] UInt32[] processIds,
+               UInt32 arraySizeBytes,
+               [MarshalAs(UnmanagedType.U4)] out UInt32 bytesCopied);
+
+        [DllImport("psapi.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern int EnumProcessModules(IntPtr hProcess, [Out] IntPtr lphModule, uint cb, out uint lpcbNeeded);
+
+        [DllImport("psapi.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+        public static extern uint GetModuleBaseName(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, uint nSize);
+
+        [DllImport("psapi.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+        public static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, uint nSize);
+    }
+}
