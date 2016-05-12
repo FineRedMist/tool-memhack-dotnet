11c2bd5c3e0cb594e68b5acf51e26e45cfb52dc4
Brent Scriver <bscriver@oneoddsock.com>
2016-05-12 01:25:25 -0400

    Move the Process List retrieval to C#. Cleanup still required.

--- ProcessTools/Processes.cs
+++ ProcessTools/Processes.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProcessTools.Windows;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.ComponentModel;
 
 namespace ProcessTools
 {
@@ -16,23 +20,339 @@ namespace ProcessTools
         /// </summary>
         public static SortedList<uint, ProcessInformation> GetProcessList(string idleProcessName, string systemName)
         {
-            return null;
+            var processes = QueryToolHelp(idleProcessName, systemName);
+            if (processes.Count == 0)
+            {
+                processes = QueryEnumProcesses(idleProcessName, systemName);
+            }
+            QueryWindows(processes);
+            QueryServices(processes);
+
+            return processes;
         }
 
+        private static SortedList<uint, ProcessInformation> QueryToolHelp(string idleProcessName, string systemName)
+        {
+            SortedList<uint, ProcessInformation> result = new SortedList<uint, ProcessInformation>();
+
+            IntPtr hSnap = Interop.INVALID_HANDLE_VALUE;
+            hSnap = Interop.CreateToolhelp32Snapshot(SnapshotFlags.Process, 0);
+            if (hSnap == IntPtr.Zero || hSnap == Interop.INVALID_HANDLE_VALUE)  // Couldn't snap, use a different method
+            {
+                return result;
+            }
+
+            PROCESSENTRY32 procEntry = new PROCESSENTRY32();
+            procEntry.dwSize = (uint) Marshal.SizeOf(procEntry);
+            if (Interop.Process32First(hSnap, ref procEntry))  // Go through each of the processes
+            {
+                do
+                {
+                    // Set up the structures
+                    ProcessInformation p = new ProcessInformation(procEntry.th32ProcessID, procEntry.szExeFile);
+                    if (p.ID == 0) // The idle process has the zero id, so save myself some work by special casing it
+                    {
+                        p.Add(new ProcessFriendlyName(idleProcessName));
+                    }
+                    else if (p.ID == 4)
+                    {
+                        p.Add(new ProcessFriendlyName(systemName));
+                    }
+                    GetProcessFullPath(p);
+                    result[p.ID] = p;
+                } while (Interop.Process32Next(hSnap, ref procEntry)); // Next process
+            }
+
+            Interop.CloseHandle(hSnap);
+            return result;
+        }
+
+        private static SortedList<uint, ProcessInformation> QueryEnumProcesses(string idleProcessName, string systemName)
+        {
+            SortedList<uint, ProcessInformation> result = new SortedList<uint, ProcessInformation>();
+
+            uint[] pdwList = new uint[1024];  // Supporting a maximum of 1024 processes at once
+            uint dwNeeded = 0;
+
+            if (!Interop.EnumProcesses(pdwList, (uint)(pdwList.Length * sizeof(uint)), out dwNeeded))
+            {
+                // dwNeeded = GetLastError();  // Mostly for debugging if I ever see it happen myself
+                return result;
+            }
+            uint dwCount = dwNeeded / sizeof(uint);
+
+            // Get the information for the list of processes
+            for (uint i = 0; i < dwCount; ++i)
+            {
+                result[pdwList[i]] = GetProcessInfo(pdwList[i], idleProcessName, systemName);
+            }
+
+            return result;
+        }
+
+        private class EnumProcessWindows
+        {
+            SortedList<uint, ProcessInformation> mProcesses;
+
+            public EnumProcessWindows(SortedList<uint, ProcessInformation> processes)
+            {
+                mProcesses = processes;
+            }
+
+            public bool EnumDesktopWindowsDelegate(IntPtr hwnd, IntPtr lParam)
+            {
+                uint dwProcess = 0;
+                uint dwThread = Interop.GetWindowThreadProcessId(hwnd, out dwProcess);
+                bool visible;
+                DateTime date;
+                string name;
+
+                if (dwThread == 0 || dwProcess == 0)    // We somehow mystically failed to get one of the thread or process ids
+                {
+                    return true;
+                }
+
+                WINDOWINFO wi = new WINDOWINFO(true);
+
+                // Get window information
+                if (!Interop.GetWindowInfo(hwnd, ref wi))
+                {
+                    return true;
+                }
+
+                // There are some classes of windows that I don't want to display so skip them
+                if (wi.dwExStyle.HasFlag(WindowExtendedStyle.ToolWindow) 
+                    || 0 != (wi.dwStyle & (WindowStyle.Popup | WindowStyle.Child | WindowStyle.Disabled)))
+                {
+                    return true;
+                }
+
+                // Get the text of the window
+                StringBuilder szBuf = new StringBuilder(1024);
+                if (0 == Interop.GetWindowText(hwnd, szBuf, szBuf.Capacity))
+                {
+                    return true;
+                }
+                name = szBuf.ToString().Trim();
+
+                // If I didn't end up with a name, drop it
+                if (name.Length == 0)
+                {
+                    return true;
+                }
+
+                // Flag whether the window is visible.  There are some that I still want to display unfortunately
+                // which means I'll also have a bunch of others that I don't want to display in the list
+                visible = (wi.dwStyle & (WindowStyle.Minimize | WindowStyle.Visible)) != 0;
+
+                long ftCreated, ftExited, ftKernel, ftUser;
+                // I use the thread information to determine when the window is created.  Typically the first window
+                // created is really the interesting one so I sort them based on date created
+                IntPtr hThread = Interop.OpenThread(ThreadAccess.QUERY_INFORMATION, false, dwThread);
+                if (hThread != IntPtr.Zero && Interop.GetThreadTimes(hThread, out ftCreated, out ftExited, out ftKernel, out ftUser))
+                {
+                    date = DateTime.FromFileTimeUtc(ftCreated);
+                }
+                else
+                {
+                    date = DateTime.FromFileTimeUtc(0);
+                }
+                if (hThread != IntPtr.Zero)
+                {
+                    Interop.CloseHandle(hThread);
+                }
+
+                ProcessFriendlyName s = new ProcessFriendlyName(name, date, visible);
+                ProcessInformation p = mProcesses[dwProcess];
+                p.Add(s);
+
+                return true;
+            }
+        }
+
+        private static void QueryWindows(SortedList<uint, ProcessInformation> processes)
+        {
+            EnumProcessWindows enumerate = new EnumProcessWindows(processes);
+            Interop.EnumDesktopWindows(Interop.GetThreadDesktop(Interop.GetCurrentThreadId()), enumerate.EnumDesktopWindowsDelegate, IntPtr.Zero);
+        }
+
+        private static void UpdateProcessInformation(SortedList<uint, ProcessInformation> processes, ENUM_SERVICE_STATUS_PROCESS serviceStatus)
+        {
+            uint processId = serviceStatus.ServiceStatus.processId;
+            if (processId == 0)
+            {
+                return;
+            }
+            ProcessInformation processInfo = null;
+            if (!processes.TryGetValue(processId, out processInfo))
+            {
+                processInfo = new ProcessInformation(processId, string.Empty);
+                processes[processId] = processInfo;
+            }
+            processInfo.Add(new ProcessFriendlyName(serviceStatus.pDisplayName));
+            processInfo.IsService = true;
+        }
+
+        private static void QueryServices(SortedList<uint, ProcessInformation> processes)
+        {
+            IntPtr handle = Interop.OpenSCManager(null, null, AccessFlags.GenericRead);
+            if (handle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            IntPtr buf = IntPtr.Zero;
+
+            try
+            {
+                uint iBytesNeeded = 0;
+                uint iServicesReturned = 0;
+                uint iResumeHandle = 0;
+
+                if (!Interop.EnumServicesStatusEx(handle, ServiceEnumType.Info, ServiceType.Win32 | ServiceType.Driver, ServiceStateRequest.All, IntPtr.Zero, 0, out iBytesNeeded, out iServicesReturned, ref iResumeHandle, null))
+                {
+                    // allocate our memory to receive the data for all the services (including the names)
+                    buf = Marshal.AllocHGlobal((int)iBytesNeeded);
+
+                    if (!Interop.EnumServicesStatusEx(handle, ServiceEnumType.Info, ServiceType.Win32 | ServiceType.Driver, ServiceStateRequest.All, buf, iBytesNeeded, out iBytesNeeded, out iServicesReturned, ref iResumeHandle, null))
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+
+                    ENUM_SERVICE_STATUS_PROCESS serviceStatus;
+
+                    // check if 64 bit system which has different pack sizes
+                    if (IntPtr.Size == 8)
+                    {
+                        long pointer = buf.ToInt64();
+                        for (int i = 0; i < (int)iServicesReturned; i++)
+                        {
+                            serviceStatus = (ENUM_SERVICE_STATUS_PROCESS)Marshal.PtrToStructure(new IntPtr(pointer), typeof(ENUM_SERVICE_STATUS_PROCESS));
+                            UpdateProcessInformation(processes, serviceStatus);
+
+                            // incremement by sizeof(ENUM_SERVICE_STATUS_PROCESS) allow Packing of 8
+                            pointer += ENUM_SERVICE_STATUS_PROCESS.SizePack8;
+                        }
+
+                    }
+                    else
+                    {
+                        int pointer = buf.ToInt32();
+                        for (int i = 0; i < (int)iServicesReturned; i++)
+                        {
+                            serviceStatus = (ENUM_SERVICE_STATUS_PROCESS)Marshal.PtrToStructure(new IntPtr(pointer), typeof(ENUM_SERVICE_STATUS_PROCESS));
+                            UpdateProcessInformation(processes, serviceStatus);
+
+                            // incremement by sizeof(ENUM_SERVICE_STATUS_PROCESS) allow Packing of 4
+                            pointer += ENUM_SERVICE_STATUS_PROCESS.SizePack4;
+                        }
+                    }
+                }
+            }
+            catch (Exception /*e*/)
+            {
+                ;
+            }
+            finally
+            {
+                if (handle != IntPtr.Zero)
+                {
+                    Interop.CloseServiceHandle(handle);
+                }
+
+                if (buf != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(buf);
+                }
+            }
+        }
+
+        private static readonly ProcessAccessFlags ProcessReadWriteFlags = ProcessAccessFlags.VirtualMemoryOperation
+            | ProcessAccessFlags.SetInformation
+            | ProcessAccessFlags.VirtualMemoryWrite
+            | ProcessAccessFlags.QueryInformation
+            | ProcessAccessFlags.VirtualMemoryRead;
+
+        private static readonly ProcessAccessFlags ProcessReadOnlyFlags = ProcessAccessFlags.QueryInformation | ProcessAccessFlags.VirtualMemoryRead;
 
         /// <summary>
         /// Acquires the full path to the process
         /// </summary>
-        private void GetProcessFullPath(ProcessInformation processInfo)
+        private static void GetProcessFullPath(ProcessInformation processInfo)
         {
+            processInfo.FullPath = null;
+            processInfo.IsService = false;
+
+            // If I succeed to open the process with the options needed to modify it, I know it is modifiable
+            IntPtr hProcess = Interop.OpenProcess(ProcessReadWriteFlags, false, processInfo.ID);
+            if (IntPtr.Zero == hProcess)  // I failed to open the process for modification so just open it to read the path info
+                hProcess = Interop.OpenProcess(ProcessReadOnlyFlags, false, processInfo.ID);
+            else // I succeeded opening the process so set the flag
+                processInfo.Modifiable = true;
+
+            if (IntPtr.Zero != hProcess)
+            {
+                IntPtr[] moduleHandles = new IntPtr[1];
+                uint cbNeeded = 0;
+                // Enumerate the modules in the process--the first one is the application which we can get the path from
+                if (Interop.EnumProcessModules(hProcess, moduleHandles, (uint)IntPtr.Size, out cbNeeded))
+                {   // Uses size in bytes
+                    StringBuilder baseName = new StringBuilder(1024);
+                    if (0 != Interop.GetModuleFileNameEx(hProcess, moduleHandles[0], baseName, (uint)baseName.Capacity))
+                    {
+                        processInfo.FullPath = baseName.ToString();
+                    }
+                }
+                Interop.CloseHandle(hProcess);
+            }
         }
 
         /// <summary>
         /// Acquires the name, path, and other details for the process
         /// </summary>
-        private ProcessInformation GetProcessInfo(uint processID, string idleProcessName, string systemName)
+        private static ProcessInformation GetProcessInfo(uint processID, string idleProcessName, string systemName)
         {
-            return null;
+            // Get a handle to the process and set some values for the process
+            string nm = (processID == 0) ? idleProcessName : ((processID == 4) ? systemName : null);
+            string fp = null;
+            bool fModifiable = false;
+
+            // Attempt to open the process with all the permissions we would need to modify it
+            // and as a backup open te process with enough information to get what we need.
+            IntPtr hProcess = Interop.OpenProcess(ProcessReadWriteFlags, false, processID);
+            if (IntPtr.Zero == hProcess)  // Failed to open the process so it isn't modifiable, backup goal
+                hProcess = Interop.OpenProcess(ProcessReadOnlyFlags, false, processID);
+            else                // Succeeded to open it so the process is modifiable
+                fModifiable = true;
+
+            // Get the process name.
+            if (IntPtr.Zero != hProcess)
+            {
+                IntPtr[] hMod = new IntPtr[1];
+                uint cbNeeded;
+
+                // The first module is (typically) the application itself, so we can get the information we need from there
+                if (Interop.EnumProcessModules(hProcess, hMod, (uint)IntPtr.Size, out cbNeeded))
+                {   // Uses size in bytes
+                    StringBuilder szProcessName = new StringBuilder(1024);
+                    StringBuilder szModulePath = new StringBuilder(1024);
+                    if (0 != Interop.GetModuleBaseName(hProcess, hMod[0], szProcessName, (uint)szProcessName.Capacity))
+                    {
+                        nm = szProcessName.ToString();
+                    }
+                    // Uses size in bytes
+                    if (0 != Interop.GetModuleFileNameEx(hProcess, hMod[0], szModulePath, (uint)szModulePath.Capacity))
+                    {
+                        fp = szModulePath.ToString();
+                    }
+                }
+
+                Interop.CloseHandle(hProcess);
+            }
+
+            ProcessInformation res = new ProcessInformation(processID, nm);
+            res.Modifiable = fModifiable;
+            res.FullPath = fp;
+
+            return res;
         }
     }
 }
--- ProcessTools/Windows/Interop.cs
+++ ProcessTools/Windows/Interop.cs
@@ -8,6 +8,12 @@ using System.Runtime.InteropServices;
 namespace ProcessTools.Windows
 {
     [Flags]
+    internal enum AccessFlags : uint
+    {
+        GenericRead = 0x80000000,
+    }
+
+    [Flags]
     internal enum ProcessAccessFlags : uint
     {
         All = 0x001F0FFF,
@@ -55,7 +61,8 @@ namespace ProcessTools.Windows
         public string szExeFile;
     };
 
-    internal delegate bool EnumDesktopWindowsDelegate(IntPtr hWnd, int lParam);
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal delegate bool EnumDesktopWindowsDelegate(IntPtr hWnd, IntPtr lParam);
 
     [StructLayout(LayoutKind.Sequential, Pack = 4)]
     internal struct ENUM_SERVICE_STATUS_PROCESS
@@ -86,17 +93,224 @@ namespace ProcessTools.Windows
         public int serviceSpecificExitCode;
         public int checkPoint;
         public int waitHint;
-        public int processId;
+        public uint processId;
         public int serviceFlags;
     }
+
+    [Flags]
+    internal enum ServiceType : uint
+    {
+        KernelDriver = 0x1,
+        FileSystemDriver = 0x2,
+        Adapter = 0x4,
+        RecognizerDriver = 0x8,
+        Win32OwnProcess = 0x10,
+        Win32ShareProcess = 0x20,
+        InteractiveProcess = 0x100,
+        Driver = (KernelDriver | FileSystemDriver | RecognizerDriver),
+        Win32 = (Win32OwnProcess | Win32ShareProcess)
+    }
+
+    [Flags]
+    internal enum ServiceStateRequest : uint
+    {
+        Active = 0x1,
+        Inactive = 0x2,
+        All = (Active | Inactive)
+    }
+
+    [Flags]
+    internal enum ServiceControlManagerType : uint
+    {
+        Connect = 0x1,
+        CreateService = 0x2,
+        EnumerateService = 0x4,
+        Lock = 0x8,
+        QueryLockStatus = 0x10,
+        ModifyBootConfig = 0x20,
+        AllAccess = /*STANDARD_RIGHTS_REQUIRED |*/ Connect | CreateService | EnumerateService | Lock | QueryLockStatus | ModifyBootConfig
+    }
+
+    [Flags]
+    internal enum ServiceEnumType : uint
+    {
+        Info = 0
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RECT
+    {
+        public int Left, Top, Right, Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
+
+        public int X
+        {
+            get { return Left; }
+            set { Right -= (Left - value); Left = value; }
+        }
+
+        public int Y
+        {
+            get { return Top; }
+            set { Bottom -= (Top - value); Top = value; }
+        }
+
+        public int Height
+        {
+            get { return Bottom - Top; }
+            set { Bottom = value + Top; }
+        }
+
+        public int Width
+        {
+            get { return Right - Left; }
+            set { Right = value + Left; }
+        }
+
+        public System.Drawing.Point Location
+        {
+            get { return new System.Drawing.Point(Left, Top); }
+            set { X = value.X; Y = value.Y; }
+        }
+
+        public System.Drawing.Size Size
+        {
+            get { return new System.Drawing.Size(Width, Height); }
+            set { Width = value.Width; Height = value.Height; }
+        }
+
+        public static implicit operator System.Drawing.Rectangle(RECT r)
+        {
+            return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
+        }
+
+        public static implicit operator RECT(System.Drawing.Rectangle r)
+        {
+            return new RECT(r);
+        }
+
+        public static bool operator ==(RECT r1, RECT r2)
+        {
+            return r1.Equals(r2);
+        }
+
+        public static bool operator !=(RECT r1, RECT r2)
+        {
+            return !r1.Equals(r2);
+        }
+
+        public bool Equals(RECT r)
+        {
+            return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RECT)
+                return Equals((RECT)obj);
+            else if (obj is System.Drawing.Rectangle)
+                return Equals(new RECT((System.Drawing.Rectangle)obj));
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ((System.Drawing.Rectangle)this).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WINDOWINFO
+    {
+        public uint cbSize;
+        public RECT rcWindow;
+        public RECT rcClient;
+        public WindowStyle dwStyle;
+        public WindowExtendedStyle dwExStyle;
+        public uint dwWindowStatus;
+        public uint cxWindowBorders;
+        public uint cyWindowBorders;
+        public ushort atomWindowType;
+        public ushort wCreatorVersion;
+
+        public WINDOWINFO(Boolean? filler) : this()   // Allows automatic initialization of "cbSize" with "new WINDOWINFO(null/true/false)".
+        {
+            cbSize = (UInt32)(Marshal.SizeOf(typeof(WINDOWINFO)));
+        }
+
+    }
+
+    [Flags]
+    internal enum ThreadAccess : int
+    {
+        TERMINATE = (0x0001),
+        SUSPEND_RESUME = (0x0002),
+        GET_CONTEXT = (0x0008),
+        SET_CONTEXT = (0x0010),
+        SET_INFORMATION = (0x0020),
+        QUERY_INFORMATION = (0x0040),
+        SET_THREAD_TOKEN = (0x0080),
+        IMPERSONATE = (0x0100),
+        DIRECT_IMPERSONATION = (0x0200)
+    }
+
+    [Flags]
+    internal enum WindowExtendedStyle : uint
+    {
+        ToolWindow = 0x00000080
+    }
+
+    [Flags]
+    internal enum WindowStyle : uint
+    {
+        Overlapped = 0x00000000,
+        Popup = 0x80000000,
+        Child = 0x40000000,
+        Minimize = 0x20000000,
+        Visible = 0x10000000,
+        Disabled = 0x08000000,
+        ClipSiblings = 0x04000000,
+        ClipChildren = 0x02000000,
+        Maximize = 0x01000000,
+        Caption = 0x00C00000,     /* WS_BORDER | WS_DLGFRAME  */
+        Border = 0x00800000,
+        DlgFrame = 0x00400000,
+        VScroll = 0x00200000,
+        HScroll = 0x00100000,
+        SysMenu = 0x00080000,
+        ThickFrame = 0x00040000,
+        Group = 0x00020000,
+        Tabstop = 0x00010000,
+    }
+
+
     internal class Interop
     {
+        public static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+
         [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, uint processId);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, uint th32ProcessID);
@@ -107,16 +321,37 @@ namespace ProcessTools.Windows
         [DllImport("kernel32.dll")]
         public static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
 
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetThreadTimes(IntPtr hThread, out long lpCreationTime,
+           out long lpExitTime, out long lpKernelTime, out long lpUserTime);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetThreadDesktop(uint dwThreadId);
+
         [DllImport("user32.dll")]
         public static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDesktopWindowsDelegate lpfn, IntPtr lParam);
 
         [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
+        public static extern IntPtr OpenSCManager(string machineName, string databaseName, AccessFlags dwAccess);
 
         [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumServicesStatusEx(IntPtr hSCManager,
-            int infoLevel, int dwServiceType,
-            int dwServiceState, IntPtr lpServices, UInt32 cbBufSize,
+            ServiceEnumType infoLevel, ServiceType dwServiceType,
+            ServiceStateRequest dwServiceState, IntPtr lpServices, UInt32 cbBufSize,
             out uint pcbBytesNeeded, out uint lpServicesReturned,
             ref uint lpResumeHandle, string pszGroupName);
 
@@ -125,12 +360,14 @@ namespace ProcessTools.Windows
         public static extern bool CloseServiceHandle(IntPtr hSCObject);
 
         [DllImport("Psapi.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumProcesses([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U4)] [In][Out] UInt32[] processIds,
                UInt32 arraySizeBytes,
                [MarshalAs(UnmanagedType.U4)] out UInt32 bytesCopied);
 
         [DllImport("psapi.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-        public static extern int EnumProcessModules(IntPtr hProcess, [Out] IntPtr lphModule, uint cb, out uint lpcbNeeded);
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumProcessModules(IntPtr hProcess, [In][Out] IntPtr[] lphModule, uint cb, out uint lpcbNeeded);
 
         [DllImport("psapi.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode)]
         public static extern uint GetModuleBaseName(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, uint nSize);
