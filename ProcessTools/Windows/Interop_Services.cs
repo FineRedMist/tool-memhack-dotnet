dba65396761050defa6d31f1e92c638f612eb4cb
Brent Scriver <bscriver@oneoddsock.com>
2016-05-12 14:06:06 -0400

    Split up some of the p/invoke methods into separate files.

---
+++ ProcessTools/Windows/Interop_Services.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace ProcessTools.Windows
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    internal struct ENUM_SERVICE_STATUS_PROCESS
+    {
+        internal static readonly int SizePack4 = Marshal.SizeOf(typeof(ENUM_SERVICE_STATUS_PROCESS));
+
+        /// <summary>
+        /// sizeof(ENUM_SERVICE_STATUS_PROCESS) allow Packing of 8 on 64 bit machines
+        /// </summary>
+        internal static readonly int SizePack8 = Marshal.SizeOf(typeof(ENUM_SERVICE_STATUS_PROCESS)) + 4;
+
+        [MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+        internal string pServiceName;
+
+        [MarshalAs(System.Runtime.InteropServices.UnmanagedType.LPWStr)]
+        internal string pDisplayName;
+
+        internal SERVICE_STATUS_PROCESS ServiceStatus;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SERVICE_STATUS_PROCESS
+    {
+        public int serviceType;
+        public int currentState;
+        public int controlsAccepted;
+        public int win32ExitCode;
+        public int serviceSpecificExitCode;
+        public int checkPoint;
+        public int waitHint;
+        public uint processId;
+        public int serviceFlags;
+    }
+
+    [Flags]
+    internal enum ServiceType : uint
+    {
+        KernelDriver = 0x1,
+        FileSystemDriver = 0x2,
+        Adapter = 0x4,
+        RecognizerDriver = 0x8,
+        Win32OwnProcess = 0x10,
+        Win32ShareProcess = 0x20,
+        InteractiveProcess = 0x100,
+        Driver = (KernelDriver | FileSystemDriver | RecognizerDriver),
+        Win32 = (Win32OwnProcess | Win32ShareProcess)
+    }
+
+    [Flags]
+    internal enum ServiceStateRequest : uint
+    {
+        Active = 0x1,
+        Inactive = 0x2,
+        All = (Active | Inactive)
+    }
+
+    [Flags]
+    internal enum ServiceControlManagerType : uint
+    {
+        Connect = 0x1,
+        CreateService = 0x2,
+        EnumerateService = 0x4,
+        Lock = 0x8,
+        QueryLockStatus = 0x10,
+        ModifyBootConfig = 0x20,
+        AllAccess = /*STANDARD_RIGHTS_REQUIRED |*/ Connect | CreateService | EnumerateService | Lock | QueryLockStatus | ModifyBootConfig
+    }
+
+    [Flags]
+    internal enum ServiceEnumType : uint
+    {
+        Info = 0
+    }
+
+    internal partial class Interop
+    {
+        public static IEnumerable<ENUM_SERVICE_STATUS_PROCESS> GetServices(
+            string machineName, string databaseName, AccessFlags accessFlags,   // From OpenSCManager
+            ServiceEnumType infoLevel, ServiceType serviceType, ServiceStateRequest serviceStateRequest // From EnumServicesStatusEx
+            )
+        {
+            List<ENUM_SERVICE_STATUS_PROCESS> results = new List<ENUM_SERVICE_STATUS_PROCESS>();
+
+            IntPtr handle = OpenSCManager(machineName, databaseName, accessFlags);
+            if (handle == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            IntPtr serviceStatusBuffer = IntPtr.Zero;
+
+            try
+            {
+                uint bytesNeeded = 0;
+                uint servicesReturned = 0;
+                uint resumeHandle = 0;
+
+                if (!EnumServicesStatusEx(handle, infoLevel, serviceType, serviceStateRequest, IntPtr.Zero, 0, out bytesNeeded, out servicesReturned, ref resumeHandle, null))
+                {
+                    // allocate our memory to receive the data for all the services (including the names)
+                    serviceStatusBuffer = Marshal.AllocHGlobal((int)bytesNeeded);
+
+                    if (!EnumServicesStatusEx(handle, infoLevel, serviceType, serviceStateRequest, serviceStatusBuffer, bytesNeeded, out bytesNeeded, out servicesReturned, ref resumeHandle, null))
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+
+                    ENUM_SERVICE_STATUS_PROCESS serviceStatus;
+
+                    // check if 64 bit system which has different pack sizes
+                    if (IntPtr.Size == 8)
+                    {
+                        long pointer = serviceStatusBuffer.ToInt64();
+                        for (int i = 0; i < (int)servicesReturned; i++)
+                        {
+                            serviceStatus = (ENUM_SERVICE_STATUS_PROCESS)Marshal.PtrToStructure(new IntPtr(pointer), typeof(ENUM_SERVICE_STATUS_PROCESS));
+                            results.Add(serviceStatus);
+
+                            // incremement by sizeof(ENUM_SERVICE_STATUS_PROCESS) allow Packing of 8
+                            pointer += ENUM_SERVICE_STATUS_PROCESS.SizePack8;
+                        }
+
+                    }
+                    else
+                    {
+                        int pointer = serviceStatusBuffer.ToInt32();
+                        for (int i = 0; i < (int)servicesReturned; i++)
+                        {
+                            serviceStatus = (ENUM_SERVICE_STATUS_PROCESS)Marshal.PtrToStructure(new IntPtr(pointer), typeof(ENUM_SERVICE_STATUS_PROCESS));
+                            results.Add(serviceStatus);
+
+                            // incremement by sizeof(ENUM_SERVICE_STATUS_PROCESS) allow Packing of 4
+                            pointer += ENUM_SERVICE_STATUS_PROCESS.SizePack4;
+                        }
+                    }
+                }
+            }
+            catch (Exception /*e*/)
+            {
+                ;
+            }
+            finally
+            {
+                if (handle != IntPtr.Zero)
+                {
+                    CloseServiceHandle(handle);
+                }
+
+                if (serviceStatusBuffer != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(serviceStatusBuffer);
+                }
+            }
+
+            return results;
+        }
+
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr OpenSCManager(string machineName, string databaseName, AccessFlags dwAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumServicesStatusEx(IntPtr hSCManager,
+            ServiceEnumType infoLevel, ServiceType dwServiceType,
+            ServiceStateRequest dwServiceState, IntPtr lpServices, UInt32 cbBufSize,
+            out uint pcbBytesNeeded, out uint lpServicesReturned,
+            ref uint lpResumeHandle, string pszGroupName);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseServiceHandle(IntPtr hSCObject);
+    }
+}
---
+++ ProcessTools/Windows/Interop_Windows.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace ProcessTools.Windows
+{
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal delegate bool EnumDesktopWindowsDelegate(IntPtr hWnd, IntPtr lParam);
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RECT
+    {
+        public int Left, Top, Right, Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
+
+        public int X
+        {
+            get { return Left; }
+            set { Right -= (Left - value); Left = value; }
+        }
+
+        public int Y
+        {
+            get { return Top; }
+            set { Bottom -= (Top - value); Top = value; }
+        }
+
+        public int Height
+        {
+            get { return Bottom - Top; }
+            set { Bottom = value + Top; }
+        }
+
+        public int Width
+        {
+            get { return Right - Left; }
+            set { Right = value + Left; }
+        }
+
+        public System.Drawing.Point Location
+        {
+            get { return new System.Drawing.Point(Left, Top); }
+            set { X = value.X; Y = value.Y; }
+        }
+
+        public System.Drawing.Size Size
+        {
+            get { return new System.Drawing.Size(Width, Height); }
+            set { Width = value.Width; Height = value.Height; }
+        }
+
+        public static implicit operator System.Drawing.Rectangle(RECT r)
+        {
+            return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
+        }
+
+        public static implicit operator RECT(System.Drawing.Rectangle r)
+        {
+            return new RECT(r);
+        }
+
+        public static bool operator ==(RECT r1, RECT r2)
+        {
+            return r1.Equals(r2);
+        }
+
+        public static bool operator !=(RECT r1, RECT r2)
+        {
+            return !r1.Equals(r2);
+        }
+
+        public bool Equals(RECT r)
+        {
+            return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RECT)
+                return Equals((RECT)obj);
+            else if (obj is System.Drawing.Rectangle)
+                return Equals(new RECT((System.Drawing.Rectangle)obj));
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ((System.Drawing.Rectangle)this).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WINDOWINFO
+    {
+        public uint cbSize;
+        public RECT rcWindow;
+        public RECT rcClient;
+        public WindowStyle dwStyle;
+        public WindowExtendedStyle dwExStyle;
+        public uint dwWindowStatus;
+        public uint cxWindowBorders;
+        public uint cyWindowBorders;
+        public ushort atomWindowType;
+        public ushort wCreatorVersion;
+
+        public WINDOWINFO(Boolean? filler) : this()   // Allows automatic initialization of "cbSize" with "new WINDOWINFO(null/true/false)".
+        {
+            cbSize = (UInt32)(Marshal.SizeOf(typeof(WINDOWINFO)));
+        }
+    }
+
+    [Flags]
+    internal enum WindowExtendedStyle : uint
+    {
+        ToolWindow = 0x00000080
+    }
+
+    [Flags]
+    internal enum WindowStyle : uint
+    {
+        Overlapped = 0x00000000,
+        Popup = 0x80000000,
+        Child = 0x40000000,
+        Minimize = 0x20000000,
+        Visible = 0x10000000,
+        Disabled = 0x08000000,
+        ClipSiblings = 0x04000000,
+        ClipChildren = 0x02000000,
+        Maximize = 0x01000000,
+        Caption = 0x00C00000,     /* WS_BORDER | WS_DLGFRAME  */
+        Border = 0x00800000,
+        DlgFrame = 0x00400000,
+        VScroll = 0x00200000,
+        HScroll = 0x00100000,
+        SysMenu = 0x00080000,
+        ThickFrame = 0x00040000,
+        Group = 0x00020000,
+        Tabstop = 0x00010000,
+    }
+
+    internal partial class Interop
+    {
+        public static string GetWindowText(IntPtr hWnd)
+        {
+            StringBuilder textBuffer = new StringBuilder(1024);
+            if (0 == GetWindowText(hWnd, textBuffer, textBuffer.Capacity))
+            {
+                return null;
+            }
+            return textBuffer.ToString();
+        }
+
+        public static Nullable<WINDOWINFO> GetWindowInfo(IntPtr hwnd)
+        {
+            WINDOWINFO windowInfo = new WINDOWINFO(true);
+            if (GetWindowInfo(hwnd, ref windowInfo))
+            {
+                return windowInfo;
+            }
+            return null;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        public static bool EnumDesktopWindows(EnumDesktopWindowsDelegate callback)
+        {
+            return EnumDesktopWindows(GetThreadDesktop(GetCurrentThreadId()), callback, IntPtr.Zero);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetThreadDesktop(uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDesktopWindowsDelegate lpfn, IntPtr lParam);
+    }
+}
