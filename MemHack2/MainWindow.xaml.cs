b9e795d10230adbbf28cdee4a428c2d167b1581d
Brent Scriver <bscriver@oneoddsock.com>
2016-05-14 00:20:38 -0400

    Add bindings for processes in the list, first set of resources for process names (idle and system).

--- MemHack2/MainWindow.xaml.cs
+++ MemHack2/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using ProcessTools;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using mkLibrary.ThemeSelector;
-using Microsoft.Win32;
-using ProcessTools;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 
 namespace MemHack2
 {
@@ -26,11 +15,15 @@ namespace MemHack2
     public partial class MainWindow : Window
     {
         private SortState mProcessSortState = new SortState();
+        private Timer mUpdateProcessListTimer = null;
 
-        public ObservableCollection<ProcessInformation> ActiveProcesses = new ObservableCollection<ProcessInformation>();
+        public ObservableCollection<ProcessInformation> ActiveProcesses { get; set; }
         
         public MainWindow()
         {
+            ActiveProcesses = new ObservableCollection<ProcessInformation>();
+            DataContext = this;
+
             InitializeComponent();
 
             var theme = GetThemeFromRegistry();
@@ -42,6 +35,117 @@ namespace MemHack2
                     cmbThemes.SelectedItem = cmbItem;
                     break;
                 }
+            }
+
+            mUpdateProcessListTimer = new Timer(UpdateProcessList, null, 0, 60 * 1000);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            mUpdateProcessListTimer.Change(Timeout.Infinite, Timeout.Infinite);
+
+            while (IsUpdateRunning)
+            {
+                Thread.Sleep(15);
+            }
+        }
+
+        private bool mIsUpdateRunning = false;
+        private object mUpdateLock = new object();
+        private bool IsUpdateRunning { get { lock (mUpdateLock) { return mIsUpdateRunning; } } }
+
+        private void UpdateProcessList(object context)
+        {
+            lock(mUpdateLock)
+            {
+                if (mIsUpdateRunning)
+                {
+                    return;
+                }
+                mIsUpdateRunning = true;
+            }
+
+            try
+            {
+                var processes = Processes.GetProcessList(Properties.Resources.IdleProcessName, Properties.Resources.SystemName);
+                Dispatcher.Invoke(() => UpdateProcessListUI(processes));
+            }
+            finally
+            {
+                lock(mUpdateLock)
+                {
+                    mIsUpdateRunning = false;
+                }
+            }
+        }
+
+        private void UpdateProcessListUI(SortedList<uint, ProcessInformation> processes)
+        {
+            var selectedProcess = RunningProcesses.SelectedItem as ProcessInformation;
+            bool findNewProcessToSelect = false;
+
+            for(int i = ActiveProcesses.Count - 1; i >= 0; --i)
+            {
+                ProcessInformation info = ActiveProcesses[i];
+                if (!processes.ContainsKey(info.ID))
+                {
+                    ActiveProcesses.RemoveAt(i);
+                    if (info == selectedProcess)
+                    {
+                        findNewProcessToSelect = true;
+                    }
+                }
+                else
+                {
+                    // Replace the instance to get any window changes.
+                    ActiveProcesses[i] = processes[info.ID];
+                    processes.Remove(info.ID);
+                    if (selectedProcess == info)
+                    {
+                        RunningProcesses.SelectedItem = ActiveProcesses[i];
+                    }
+                }
+            }
+
+            if (processes.Count > 0)
+            {
+                int currentProcessIndex = 0;
+                ProcessInformation infoToInsert = processes.Values[currentProcessIndex];
+
+                for (int i = 0; i < ActiveProcesses.Count; ++i)
+                {
+                    ProcessInformation current = ActiveProcesses[i];
+                    if (infoToInsert.ID < current.ID)
+                    {
+                        ActiveProcesses.Insert(i, infoToInsert);
+                        ++currentProcessIndex;
+                        if (currentProcessIndex >= processes.Count)
+                        {
+                            break;
+                        }
+                        infoToInsert = processes.Values[currentProcessIndex];
+                    }
+                }
+
+                for (; currentProcessIndex < processes.Count; ++currentProcessIndex)
+                {
+                    infoToInsert = processes.Values[currentProcessIndex];
+                    ActiveProcesses.Add(infoToInsert);
+                }
+            }
+
+            if (findNewProcessToSelect && ActiveProcesses.Count > 0)
+            {
+                ProcessInformation toSelect = ActiveProcesses[ActiveProcesses.Count - 1];
+                foreach (var proc in ActiveProcesses)
+                {
+                    if (proc.ID > selectedProcess.ID)
+                    {
+                        toSelect = proc;
+                        break;
+                    }
+                }
+                RunningProcesses.SelectedItem = toSelect;
             }
         }
 
@@ -102,9 +206,9 @@ namespace MemHack2
 
         }
 
-        private void RunningProcesses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Refresh_Click(object sender, RoutedEventArgs e)
         {
-
+            ThreadPool.QueueUserWorkItem(UpdateProcessList);
         }
     }
 
